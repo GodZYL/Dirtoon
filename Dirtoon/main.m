4781d82ecf51e5abec847dcc6b254e7898767e36
godloong <godloong@prodemacbook-pro.local>
2018-12-26 13:51:07 +0800

    Initial Commit

---
+++ Dirtoon/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Dirtoon
+//
+//  Created by godloong on 2018/12/26.
+//  Copyright © 2018 godloong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
