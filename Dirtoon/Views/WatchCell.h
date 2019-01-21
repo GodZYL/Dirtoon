72aa2c45b5353617b6bafa2d4134aca73db9c222
赵云龙 <a18167547@qq.com>
2019-01-21 09:15:56 +0800

    v1.1

---
+++ Dirtoon/Views/WatchCell.h
@@ -0,0 +1,17 @@
+//
+//  WatchCell.h
+//  Dirtoon
+//
+//  Created by godloong on 2019/1/19.
+//  Copyright © 2019 godloong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WatchCell : UICollectionViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Dirtoon/Views/WatchCell.m
@@ -0,0 +1,13 @@
+//
+//  WatchCell.m
+//  Dirtoon
+//
+//  Created by godloong on 2019/1/19.
+//  Copyright © 2019 godloong. All rights reserved.
+//
+
+#import "WatchCell.h"
+
+@implementation WatchCell
+
+@end
